7ed675e1d384b2757ec54d36f06e4cee164d0ce1
mnaufalmuafa <naufalmuafa77@gmail.com>
2021-04-17 11:57:00 +0700

    Menambahkan poster dan video YT

---
+++ index.js
@@ -0,0 +1,12 @@
+$("document").ready(function() {
+  var win_width = window.innerWidth;
+
+  if (win_width < 800){
+    $("#default-container").hide();
+    // $("footer").css("margin-top","330px");
+  }
+  else {
+    $("#responsive-container").hide();
+  }
+
+});
