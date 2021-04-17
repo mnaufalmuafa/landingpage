79c8352816057c17dacb09506623d8fd9b5388c7
mnaufalmuafa <naufalmuafa77@gmail.com>
2021-04-17 15:17:06 +0700

    Menghandle perubahan ukuran window

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 $("document").ready(function() {
   var win_width = window.innerWidth;
 
-  if (win_width < 800){
-    $("#default-container").hide();
-    $("footer").css("margin-top","330px");
+  $("#default-container").hide();
+  $("#responsive-container").hide();
+
+  if (win_width > 800){
+    $("#default-container").show();
   }
   else {
-    $("#responsive-container").hide();
+    $("#responsive-container").show();
+    $("footer").css("margin-top","330px");
+  }
+
+  window.onresize = function() {
+    let win_width = window.innerWidth;
+    
+    if (win_width > 800){
+      $("#default-container").show();
+      $("#responsive-container").hide();
+      $("footer").css("margin-top","15px");
+    }
+    else {
+      $("#responsive-container").show();
+      $("#default-container").hide();
+      $("footer").css("margin-top","330px");
+    }
   }
 
 });
